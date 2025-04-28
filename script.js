a55da7952fd5400d31e0cc2dc5e38439320e8a48
Miles-sys-1 <muxammadjonmominov333@gmail.com>
2025-04-28 09:00:32 +0500

    my website

---
+++ script.js
@@ -0,0 +1,15 @@
+// script.js
+document.querySelectorAll('a.nav-link').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        if (this.hash !== '') {
+            e.preventDefault();
+            const target = document.querySelector(this.hash);
+            if (target) {
+                target.scrollIntoView({
+                    behavior: 'smooth',
+                    block: 'start'
+                });
+            }
+        }
+    });
+});
